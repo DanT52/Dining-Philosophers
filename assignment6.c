d4d277fb8349780f308bded5ae62f1424d0ea4b2
Daniel Basarab <dantramp52@gmail.com>
2023-10-18 22:47:59 +0000

    working threads version.

---
+++ assignment6.c
@@ -0,0 +1,151 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define PHILOSOPHERS 5
+#define EATING_TIME 10
+
+
+typedef struct {
+    int thinkTimeTotal;
+    int eatTimeTotal;
+    int cycles;
+} PhilosopherData;
+
+typedef struct {
+    PhilosopherData *data;
+    int num;
+    pthread_mutex_t *mutexes;
+} ThreadArgs;
+
+//rng function
+int randomGaussian(int mean, int stddev) {
+    double mu = 0.5 + (double) mean;
+    double sigma = fabs((double) stddev);
+    double f1 = sqrt(-2.0 * log((double) rand() / (double) RAND_MAX));
+    double f2 = 2.0 * 3.14159265359 * (double) rand() / (double) RAND_MAX;
+    if (rand() & (1 << 5)) 
+        return (int) floor(mu + sigma * cos(f2) * f1);
+    else            
+        return (int) floor(mu + sigma * sin(f2) * f1);
+}
+
+
+
+void *printMessage(void *message) {
+    printf("%s\n", (char *)message);
+    return NULL;
+}
+
+
+void *philosopher_thread(void *args){
+    //take apart the args
+    ThreadArgs *threadArgs = (ThreadArgs *)args;
+    pthread_mutex_t *mutex = threadArgs->mutexes;
+    PhilosopherData *data = threadArgs->data;
+    int num = threadArgs->num;
+
+	//seed for the rng
+	srand(time(NULL) + num);
+    int eatTimeTotal = 0, thinkTimeTotal = 0, cycles = 0;
+
+	while (eatTimeTotal < EATING_TIME) {
+                cycles++;
+                
+                // thinking phase
+                int thinkTime = randomGaussian(11, 7);
+                if (thinkTime < 0) thinkTime = 0;
+                printf("Philosopher %d thinking for %d seconds (total = %d)\n", num, thinkTime, thinkTimeTotal);
+				thinkTimeTotal += thinkTime;
+                sleep(thinkTime);
+
+				// trying to pick up chopsticks
+				if(pthread_mutex_trylock(&threadArgs->mutexes[num]) == 0){
+                    if (pthread_mutex_trylock(&threadArgs->mutexes[(num+1)%PHILOSOPHERS]) == 0){
+                        int eatTime = randomGaussian(9, 3);
+                        if (eatTime < 0) eatTime = 0;
+
+                        printf("Philosopher %d eating for %d seconds (total = %d)\n", num, eatTime, eatTimeTotal);
+                        eatTimeTotal += eatTime;
+                        sleep(eatTime);
+
+                        pthread_mutex_unlock(&threadArgs->mutexes[(num+1)%PHILOSOPHERS]);
+                        
+                            
+                    }
+                        
+                    pthread_mutex_unlock(&threadArgs->mutexes[num]);
+                }
+                
+                // eating phase
+                
+            }
+            //save details about process. print that meal was finished.
+            printf("Philosopher %d done with meal\n", num);
+            data->thinkTimeTotal = thinkTimeTotal;
+            data->eatTimeTotal = eatTimeTotal;
+            data->cycles = cycles;
+            
+}
+
+void init_mutexes(pthread_mutex_t *mutexes){
+    for(int i = 0; i < PHILOSOPHERS; i++){
+        pthread_mutex_init(&mutexes[i], NULL);
+    }
+}
+
+void end_mutexes(pthread_mutex_t *mutexes){
+    for(int i = 0; i < PHILOSOPHERS; i++){
+        pthread_mutex_destroy(&mutexes[i]);
+
+    }
+    free(mutexes);
+}
+
+int main(){
+    pthread_t philos_thread[PHILOSOPHERS];
+
+    //initialize mutexes
+    pthread_mutex_t *mutexes = malloc(PHILOSOPHERS * sizeof(pthread_mutex_t));
+    init_mutexes(mutexes);
+
+    //allocate philosopher data
+    PhilosopherData *data = malloc(PHILOSOPHERS * sizeof(PhilosopherData));
+
+    ThreadArgs *threadArgs[PHILOSOPHERS];
+
+    for (int i = 0; i< PHILOSOPHERS; i++){
+        threadArgs[i] = malloc(sizeof(ThreadArgs));
+        threadArgs[i]->mutexes = mutexes;
+        threadArgs[i]->data = &data[i];
+        threadArgs[i]->num =i;
+
+        int result = pthread_create(&philos_thread[i], NULL, philosopher_thread, (void*)threadArgs[i]);
+
+    }
+
+    for(int i = 0; i < PHILOSOPHERS; i++){
+        pthread_join(philos_thread[i], NULL);
+    }
+
+
+    //print results
+    printf("\n");
+    for(int i = 0; i < PHILOSOPHERS; i++){
+        printf("Philosopher %d thought for %d seconds, ate for %d seconds over %d cycles.\n", 
+               i, data[i].thinkTimeTotal, data[i].eatTimeTotal, data[i].cycles);
+    }
+
+
+    printf("done\n");
+
+
+
+    end_mutexes(mutexes);
+    free(data);
+
+}
