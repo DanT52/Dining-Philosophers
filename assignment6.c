675175c5261f824d9f00f0274f9d6d750a816553
Daniel Basarab <dantramp52@gmail.com>
2023-10-24 00:27:17 +0000

    errchks and more functions

--- assignment6.c
+++ assignment6.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 #define PHILOSOPHERS 5
 #define EATING_TIME 100
@@ -34,15 +36,26 @@ int randomGaussian(int mean, int stddev) {
         return (int) floor(mu + sigma * sin(f2) * f1);
 }
 
-
-
-void *printMessage(void *message) {
-    printf("%s\n", (char *)message);
-    return NULL;
+//helper function for picking up chopsticks, waits for both to be avalible in a busy loop before picking up.
+//takes in the thread args and the philosopher num.
+void pickup_chopsticks(ThreadArgs *threadArgs, int num) {
+    int first = 1;
+    while (1) {
+        if (pthread_mutex_trylock(&threadArgs->mutexes[num]) == 0) {
+            if (pthread_mutex_trylock(&threadArgs->mutexes[(num+1)%PHILOSOPHERS]) == 0) {
+                break;
+            }
+            pthread_mutex_unlock(&threadArgs->mutexes[num]);
+        }
+        if (first) {
+            printf("Philosopher %d waiting for sticks %d and %d\n", num, num, (num+1) % PHILOSOPHERS);
+            first = 0;
+        }
+    }
 }
 
-
-void *philosopher_thread(void *args){
+//philisopher thread function, runs each philosopher through their eating/thinking cycle till they are done.
+void *philosopher_thread(void *args){ 
     //take apart the args
     ThreadArgs *threadArgs = (ThreadArgs *)args;
     pthread_mutex_t *mutex = threadArgs->mutexes;
@@ -57,30 +70,16 @@ void *philosopher_thread(void *args){
                 cycles++;
                 
                 // thinking phase
-                int thinkTime = 0; //randomGaussian(11, 7);
+                int thinkTime = randomGaussian(11, 7);
                 if (thinkTime < 0) thinkTime = 0;
                 printf("Philosopher %d thinking for %d seconds (total = %d)\n", num, thinkTime, thinkTimeTotal);
 				thinkTimeTotal += thinkTime;
                 sleep(thinkTime);
 
-				// trying to pick up chopsticks
-                int first = 1;
-                while(1){
-                    if(pthread_mutex_trylock(&threadArgs->mutexes[num]) == 0){
-                        if (pthread_mutex_trylock(&threadArgs->mutexes[(num+1)%PHILOSOPHERS]) == 0){
-                            break;
-                        }
-                        pthread_mutex_unlock(&threadArgs->mutexes[num]);
-                    }
-                    if (first){
-                        printf("Philosopher %d waiting for sticks %d and %d\n", num, num, (num+1) % PHILOSOPHERS);
-                        first = 0;
-                    }
-                    
-                }
+				pickup_chopsticks(threadArgs, num); //pickup chops
                 
                 // eating phase
-                int eatTime = 0;//randomGaussian(9, 3);
+                int eatTime = randomGaussian(9, 3);
                 if (eatTime < 0) eatTime = 0;
 
                 printf("Philosopher %d eating for %d seconds (total = %d)\n", num, eatTime, eatTimeTotal);
@@ -96,21 +95,66 @@ void *philosopher_thread(void *args){
             data->thinkTimeTotal = thinkTimeTotal;
             data->eatTimeTotal = eatTimeTotal;
             data->cycles = cycles;
-            
 }
 
-void init_mutexes(pthread_mutex_t *mutexes){
-    for(int i = 0; i < PHILOSOPHERS; i++){
-        pthread_mutex_init(&mutexes[i], NULL);
+//initialize and return array of mutexes with one for each chopstick
+pthread_mutex_t* init_mutexes(){
+    pthread_mutex_t *mutexes = malloc(PHILOSOPHERS * sizeof(pthread_mutex_t));
+    if (!mutexes) {
+        fprintf(stderr, "ERROR: Failed to allocate memory for mutexes '%s' ERRNO: %d", strerror(errno), errno );
+        exit(1);
     }
+
+    for(int i = 0; i < PHILOSOPHERS; i++){
+        if (pthread_mutex_init(&mutexes[i], NULL)){
+            fprintf(stderr, "ERROR: Failed init mutexes '%s' ERRNO: %d", strerror(errno), errno );
+            exit(1);
+        }
+    }
+    return mutexes;
 }
 
+//free mutexes
 void end_mutexes(pthread_mutex_t *mutexes){
     for(int i = 0; i < PHILOSOPHERS; i++){
-        pthread_mutex_destroy(&mutexes[i]);
-
+        if (pthread_mutex_destroy(&mutexes[i]))fprintf(stderr, "ERROR: Failed destroy mutexes '%s' ERRNO: %d", strerror(errno), errno );
     }
     free(mutexes);
+}
+
+// Function for running philosophers cycle
+//creates thread for each philosopher that runs till they finish eating
+// args: pthread array (one for each philosopher), mutex array with a mutex for each chopstick
+//returns data array with data about the simulation.
+PhilosopherData* run_philosophers(pthread_t *philos_thread, pthread_mutex_t *mutexes){
+
+    PhilosopherData *data = malloc(PHILOSOPHERS * sizeof(PhilosopherData));
+    if (!data){
+            fprintf(stderr, "ERROR: failed to allocate data '%s' ERRNO: %d", strerror(errno), errno );
+            exit(1);
+    }
+    ThreadArgs *threadArgs[PHILOSOPHERS];
+
+    for (int i = 0; i< PHILOSOPHERS; i++){ //create thread for each philosopher
+        threadArgs[i] = malloc(sizeof(ThreadArgs));
+        if (!threadArgs[i]){
+            fprintf(stderr, "ERROR: failed to allocate args '%s' ERRNO: %d", strerror(errno), errno );
+            exit(1);
+        }
+        threadArgs[i]->mutexes = mutexes;
+        threadArgs[i]->data = &data[i];
+        threadArgs[i]->num =i; //philosopher number
+        int result = pthread_create(&philos_thread[i], NULL, philosopher_thread, (void*)threadArgs[i]); 
+    }
+
+    for(int i = 0; i < PHILOSOPHERS; i++){ //wait for philosophers to finish
+        if (pthread_join(philos_thread[i], NULL) != 0){
+            fprintf(stderr, "ERROR: pthread_join failed '%s' ERRNO: %d", strerror(errno), errno );
+            exit(1);
+        }
+        free(threadArgs[i]); 
+    }
+    return data;
 }
 
 int main(){
@@ -119,30 +163,8 @@ int main(){
     gettimeofday(&start, NULL);	//get time to display total time ran at end.
 
     pthread_t philos_thread[PHILOSOPHERS];
-
-    //initialize mutexes
-    pthread_mutex_t *mutexes = malloc(PHILOSOPHERS * sizeof(pthread_mutex_t));
-    init_mutexes(mutexes);
-
-    //allocate philosopher data
-    PhilosopherData *data = malloc(PHILOSOPHERS * sizeof(PhilosopherData));
-
-    ThreadArgs *threadArgs[PHILOSOPHERS];
-
-    for (int i = 0; i< PHILOSOPHERS; i++){
-        threadArgs[i] = malloc(sizeof(ThreadArgs));
-        threadArgs[i]->mutexes = mutexes;
-        threadArgs[i]->data = &data[i];
-        threadArgs[i]->num =i;
-
-        int result = pthread_create(&philos_thread[i], NULL, philosopher_thread, (void*)threadArgs[i]);
-
-    }
-
-    for(int i = 0; i < PHILOSOPHERS; i++){
-        pthread_join(philos_thread[i], NULL);
-    }
-
+    pthread_mutex_t *mutexes = init_mutexes();
+    PhilosopherData *data = run_philosophers(philos_thread, mutexes);
 
     //print results
     printf("-\n");
@@ -150,12 +172,9 @@ int main(){
         printf("Philosopher %d thought for %d seconds, ate for %d seconds over %d cycles.\n", 
                i, data[i].thinkTimeTotal, data[i].eatTimeTotal, data[i].cycles);
     }
-
     gettimeofday(&end, NULL);
     long int timePassed = end.tv_sec - start.tv_sec;
     printf("Program took %ld seconds Total to execute \n", timePassed);
-
-
 
     end_mutexes(mutexes);
     free(data);
